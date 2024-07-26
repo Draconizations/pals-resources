bcb844509d53d24511cff4859ac3419b4d9a3cb3
Jake Fulmine <jake@fulmine.xyz>
2024-07-26 14:56:20 +0200

    gah??

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://resources.pals.gay/"
+  site: "https://resources.pals.gay"
 });
